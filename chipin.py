17588348fa3526d2f7ddf7210482a346e6c1213b
Félix An <cutekids100@gmail.com>
2021-02-22 18:28:57 -0500

    Add files via upload

--- chipin.py
+++ chipin.py
@@ -5,15 +5,15 @@
 import pinyin   # You must install this module using pip
 import csv
 
-inFile = open("input.txt", "r")
+inFile = open("input.txt", "r", encoding="utf-8")
 characters = inFile.readlines()
 inFile.close()
 
 characters = [ele.strip() for ele in characters]
 
 for ca, xa in enumerate(characters):
-    characters[ca] = [characters[ca], pinyin.get(characters[ca])]
+    characters[ca] = [characters[ca], pinyin.get(characters[ca], format="diacritical", delimiter=' ')]
 
-with open("output.csv", "w") as csvFile:
-    writer = csv.writer(csvFile)
+with open("output.csv", "w", encoding="utf-8") as csvFile:
+    writer = csv.writer(csvFile, lineterminator='\n')
     writer.writerows(characters)
